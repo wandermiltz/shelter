eeb8ae14f1b74fac686a53c73a9aa506e3c41066
Olga Berezka <olga.berezka.main@gmail.com>
2023-04-08 11:59:10 +0100

    feat: stop page scroll if nav menu opened

--- shelter/js/navigation-burger.js
+++ shelter/js/navigation-burger.js
@@ -2,17 +2,20 @@
 	const burger = document.querySelector('.navigation__burger');
 	const nav = document.querySelector('.navigation__list');
 	const darken = document.querySelector('.darken');
+	const body = document.querySelector('body');
 
 	function toggleMenu() {
 		burger.classList.toggle('navigation__burger_active');
 		nav.classList.toggle('navigation__list_active');
 		darken.classList.toggle('darken_active');
+		body.classList.toggle('stop-scroll');
 	}
 
 	function removeMenu() {
 		burger.classList.remove('navigation__burger_active');
 		nav.classList.remove('navigation__list_active');
 		darken.classList.remove('darken_active');
+		body.classList.remove('stop-scroll');
 	}
 
 	document.addEventListener('click', el => {
