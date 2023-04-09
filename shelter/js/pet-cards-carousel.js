61aa7e88ff9daaed02299285aaae2b369ae8a697
Olga Berezka <olga.berezka.main@gmail.com>
2023-04-09 13:38:04 +0100

    feat: add different generateFullPetCardsSet according media queries

--- shelter/js/pet-cards-carousel.js
+++ shelter/js/pet-cards-carousel.js
@@ -30,15 +30,15 @@ async function generatePetCardHtml(petCardIndex) {
 
 async function insertPetCardHtml(petCardIndex) {
 	let petCardHtml = await generatePetCardHtml(petCardIndex);
-	petCardsContainer.insertAdjacentHTML('afterbegin', petCardHtml)
+	petCardsContainer.insertAdjacentHTML('afterbegin', petCardHtml);
 }
 
 function getRandomNum(max, min) {
-	return Math.floor(min + Math.random() * (max + 1 - min))
+	return Math.floor(min + Math.random() * (max + 1 - min));
 }
 
 function getRandomSequenceCompared(min, max, sequenceLength, arrToCompare = []) {
-	let numArr = []
+	let numArr = [];
 	while (numArr.length !== sequenceLength) {
 		let num = getRandomNum(max, min);
 		if ((numArr.indexOf(num) !== - 1) || (arrToCompare.indexOf(num) !== - 1)) continue;
@@ -54,11 +54,11 @@ function getInitCardSet() {
 	let leftCardSet = [...currCardSet];
 	console.log('left', leftCardSet)
 	currCardSet = [...rightCardSet];
-	console.log('curr', currCardSet)
+	console.log('curr', currCardSet);
 	rightCardSet = getRandomSequenceCompared(0, 7, 3, currCardSet);
-	console.log('right', rightCardSet)
+	console.log('right', rightCardSet);
 
-	return [leftCardSet, currCardSet, rightCardSet]
+	return [leftCardSet, currCardSet, rightCardSet];
 }
 
 function animateMoveCardRight() {
@@ -85,62 +85,62 @@ function moveCardsByRightButton(leftCardSet, currCardSet, rightCardSet) {
 
 	// left <- curr
 	leftCardSet = [...currCardSet];
-	console.log('left', leftCardSet)
+	console.log('left', leftCardSet);
 	// curr <- right
 	currCardSet = [...rightCardSet];
-	console.log('curr', currCardSet)
+	console.log('curr', currCardSet);
 	// generate new right
 	rightCardSet = getRandomSequenceCompared(0, 7, 3, currCardSet);
-	console.log('right', rightCardSet)
+	console.log('right', rightCardSet);
 
 	return [leftCardSet, currCardSet, rightCardSet]
 }
 
 function moveCardsByLeftButton(leftCardSet, currCardSet, rightCardSet) {
 
-	animateMoveCardLeft()
+	animateMoveCardLeft();
 
 	// curr -> right
 	rightCardSet = [...currCardSet];
-	console.log('left', leftCardSet)
+	console.log('left', leftCardSet);
 	// left -> curr
 	currCardSet = [...leftCardSet];
-	console.log('curr', currCardSet)
+	console.log('curr', currCardSet);
 	// generate new left
 	leftCardSet = getRandomSequenceCompared(0, 7, 3, currCardSet);
-	console.log('right', leftCardSet)
+	console.log('right', leftCardSet);
 
 	return [leftCardSet, currCardSet, rightCardSet]
 }
 
-let fullCardSetOuter = getInitCardSet()
-let currCardSetOuter = fullCardSetOuter[1]
-console.log('currOuter', currCardSetOuter)
+let fullCardSetOuter = getInitCardSet();
+let currCardSetOuter = fullCardSetOuter[1];
+console.log('currOuter', currCardSetOuter);
 
 currCardSetOuter.forEach(el => {
-	insertPetCardHtml(el)
+	insertPetCardHtml(el);
 })
 
 rightCarouselButton.addEventListener('click', el => {
-	let cardSetMovedByRightButton = moveCardsByRightButton(...fullCardSetOuter)
-	console.log('movedRight', cardSetMovedByRightButton)
-	petCardsContainer.innerHTML = ''
-	let currCardSetMoved = cardSetMovedByRightButton[1]
-	console.log('currCardSetMovedRight', cardSetMovedByRightButton[1])
+	let cardSetMovedByRightButton = moveCardsByRightButton(...fullCardSetOuter);
+	console.log('movedRight', cardSetMovedByRightButton);
+	petCardsContainer.innerHTML = '';
+	let currCardSetMoved = cardSetMovedByRightButton[1];
+	console.log('currCardSetMovedRight', cardSetMovedByRightButton[1]);
 	currCardSetMoved.forEach(el => {
-		insertPetCardHtml(el)
+		insertPetCardHtml(el);
 	})
-	fullCardSetOuter = cardSetMovedByRightButton
+	fullCardSetOuter = cardSetMovedByRightButton;
 })
 
 leftCarouselButton.addEventListener('click', el => {
-	let cardSetMovedByLeftButton = moveCardsByLeftButton(...fullCardSetOuter)
-	console.log('movedLeft', cardSetMovedByLeftButton)
-	let currCardSetMoved = cardSetMovedByLeftButton[1]
-	console.log('currCardSetMovedLeft', cardSetMovedByLeftButton[1])
-	petCardsContainer.innerHTML = ''
+	let cardSetMovedByLeftButton = moveCardsByLeftButton(...fullCardSetOuter);
+	console.log('movedLeft', cardSetMovedByLeftButton);
+	let currCardSetMoved = cardSetMovedByLeftButton[1];
+	console.log('currCardSetMovedLeft', cardSetMovedByLeftButton[1]);
+	petCardsContainer.innerHTML = '';
 	currCardSetMoved.forEach(el => {
-		insertPetCardHtml(el)
+		insertPetCardHtml(el);
 	})
-	fullCardSetOuter = cardSetMovedByLeftButton
+	fullCardSetOuter = cardSetMovedByLeftButton;
 })
--- shelter/js/pet-cards-pagination.js
+++ shelter/js/pet-cards-pagination.js
@@ -31,126 +31,150 @@ async function generatePetCardHtml(obj) {
 }
 
 function getRandomNum(max, min) {
-	return Math.floor(min + Math.random() * (max + 1 - min))
+	return Math.floor(min + Math.random() * (max + 1 - min));
 }
 
 function getRandomSequence(min, max, sequenceLength) {
-	let numArr = []
+	let numArr = [];
 	while (numArr.length !== sequenceLength) {
 		let num = getRandomNum(max, min);
 		if (numArr.indexOf(num) !== - 1) continue;
 		numArr.push(num);
 	}
-	console.log(numArr)
 	return numArr;
 }
 
-async function generateFullPetCardsSet() {
+async function generateFullPetCardsSet(maxPageIndex, cardsPerPageCount) {
 
 	const data = await getPetCardsData();
 	let arrOfCardSequences = []
 
-	for (let i = 0; i < 6; i++) {
-		let randomSequence = getRandomSequence(0, 7, 8)
-		let arrOfRandomCards = []
-		for (let i = 0; i < data.length; i++) {
-			arrOfRandomCards.push(data[randomSequence[i]])
+	for (let i = 0; i <= maxPageIndex; i++) {
+		let randomSequence = getRandomSequence(0, 7, cardsPerPageCount);
+		let arrOfRandomCards = [];
+		for (let i = 0; i < cardsPerPageCount; i++) {
+			arrOfRandomCards.push(data[randomSequence[i]]);
 		}
-		arrOfCardSequences.push(arrOfRandomCards)
+		arrOfCardSequences.push(arrOfRandomCards);
 	}
-	console.log(arrOfCardSequences)
-	return arrOfCardSequences
+	return arrOfCardSequences;
 }
-
-let fullPetCardsSet = await generateFullPetCardsSet()
-console.log(fullPetCardsSet)
 
 async function insertPetCardHtml(pageIndex) {
 	for (let j = 0; j < fullPetCardsSet[pageIndex].length; j++) {
-		let petCardHtml = await generatePetCardHtml(fullPetCardsSet[pageIndex][j])
-		petCardsContainer.insertAdjacentHTML('afterbegin', petCardHtml)
+		let petCardHtml = await generatePetCardHtml(fullPetCardsSet[pageIndex][j]);
+		petCardsContainer.insertAdjacentHTML('afterbegin', petCardHtml);
 	}
 }
 
-let pageIndex = 0
-let maxPageIndex = 5
-insertPetCardHtml(pageIndex)
+let pageIndex = 0;
+let maxPageIndex = 0;
+let cardsPerPageCount = 0;
+let fullPetCardsSet = [];
+
+if (window.innerWidth < 768) {
+	console.log('check on 320px');
+	maxPageIndex = 15;
+	cardsPerPageCount = 3;
+
+	fullPetCardsSet = await generateFullPetCardsSet(maxPageIndex, cardsPerPageCount);
+	console.log('fullPetCardsSet', fullPetCardsSet);
+	insertPetCardHtml(pageIndex);
+
+} else if (window.innerWidth <= 1200) {
+	console.log('check on 768px');
+	maxPageIndex = 7;
+	cardsPerPageCount = 6;
+
+	fullPetCardsSet = await generateFullPetCardsSet(maxPageIndex, cardsPerPageCount);
+	console.log('fullPetCardsSet', fullPetCardsSet);
+	insertPetCardHtml(pageIndex);
+
+} else {
+	console.log('check on 1280px');
+	maxPageIndex = 5;
+	cardsPerPageCount = 8;
+
+	fullPetCardsSet = await generateFullPetCardsSet(maxPageIndex, cardsPerPageCount);
+	console.log('fullPetCardsSet', fullPetCardsSet);
+	insertPetCardHtml(pageIndex);
+}
 
 function disableBtn(btn) {
 	btn.disabled = true;
-	btn.classList.add('pg-button_disabled')
-	btn.classList.remove('pg-button_normal')
+	btn.classList.add('pg-button_disabled');
+	btn.classList.remove('pg-button_normal');
 }
 
 function enableBtn(btn) {
 	btn.disabled = false;
-	btn.classList.remove('pg-button_disabled')
-	btn.classList.add('pg-button_normal')
+	btn.classList.remove('pg-button_disabled');
+	btn.classList.add('pg-button_normal');
 }
 
 pgBtnRight.addEventListener('click', el => {
 	if (pageIndex < maxPageIndex) {
-		pageIndex += 1
-		petCardsContainer.innerHTML = ''
-		insertPetCardHtml(pageIndex)
-		pageNumber.innerHTML = `${pageIndex + 1}`
+		pageIndex += 1;
+		petCardsContainer.innerHTML = '';
+		insertPetCardHtml(pageIndex);
+		pageNumber.innerHTML = `${pageIndex + 1}`;
 
-		enableBtn(pgBtnLeft)
-		enableBtn(pgBtnToFirst)
+		enableBtn(pgBtnLeft);
+		enableBtn(pgBtnToFirst);
 
 		if (pageIndex == maxPageIndex) {
-			disableBtn(pgBtnRight)
-			disableBtn(pgBtnToLast)
+			disableBtn(pgBtnRight);
+			disableBtn(pgBtnToLast);
 		}
 	} else {
-		disableBtn(pgBtnRight)
+		disableBtn(pgBtnRight);
 	}
 })
 
 pgBtnLeft.addEventListener('click', el => {
 	if (pageIndex > 0 && pageIndex <= maxPageIndex) {
 		pageIndex -= 1
-		petCardsContainer.innerHTML = ''
-		insertPetCardHtml(pageIndex)
-		pageNumber.innerHTML = `${pageIndex + 1}`
+		petCardsContainer.innerHTML = '';
+		insertPetCardHtml(pageIndex);
+		pageNumber.innerHTML = `${pageIndex + 1}`;
 
-		enableBtn(pgBtnLeft)
-		enableBtn(pgBtnRight)
-		enableBtn(pgBtnToFirst)
-		enableBtn(pgBtnToLast)
+		enableBtn(pgBtnLeft);
+		enableBtn(pgBtnRight);
+		enableBtn(pgBtnToFirst);
+		enableBtn(pgBtnToLast);
 
 		if (pageIndex == 0) {
-			disableBtn(pgBtnLeft)
-			disableBtn(pgBtnToFirst)
+			disableBtn(pgBtnLeft);
+			disableBtn(pgBtnToFirst);
 		}
 	} else {
-		disableBtn(pgBtnLeft)
+		disableBtn(pgBtnLeft);
 	}
 })
 
 pgBtnToFirst.addEventListener('click', el => {
 	pageIndex = 0
-	petCardsContainer.innerHTML = ''
-	insertPetCardHtml(pageIndex)
-	pageNumber.innerHTML = `${pageIndex + 1}`
+	petCardsContainer.innerHTML = '';
+	insertPetCardHtml(pageIndex);
+	pageNumber.innerHTML = `${pageIndex + 1}`;
 
-	disableBtn(pgBtnLeft)
-	disableBtn(pgBtnToFirst)
+	disableBtn(pgBtnLeft);
+	disableBtn(pgBtnToFirst);
 
-	enableBtn(pgBtnRight)
-	enableBtn(pgBtnToLast)
+	enableBtn(pgBtnRight);
+	enableBtn(pgBtnToLast);
 })
 
 
 pgBtnToLast.addEventListener('click', el => {
-	pageIndex = maxPageIndex
-	petCardsContainer.innerHTML = ''
-	insertPetCardHtml(pageIndex)
-	pageNumber.innerHTML = `${pageIndex + 1}`
+	pageIndex = maxPageIndex;
+	petCardsContainer.innerHTML = '';
+	insertPetCardHtml(pageIndex);
+	pageNumber.innerHTML = `${pageIndex + 1}`;
 
-	disableBtn(pgBtnRight)
-	disableBtn(pgBtnToLast)
+	disableBtn(pgBtnRight);
+	disableBtn(pgBtnToLast);
 
-	enableBtn(pgBtnLeft)
-	enableBtn(pgBtnToFirst)
+	enableBtn(pgBtnLeft);
+	enableBtn(pgBtnToFirst);
 })
